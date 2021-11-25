408c8ab3c5ec98cbaff788c7b3d033c176c74f61
razerx100 <saikatmandal8@gmail.com>
2021-11-26 02:43:57 +0600

    added const specifier

--- exports/IModel.hpp
+++ exports/IModel.hpp
@@ -9,10 +9,10 @@ public:
 
 	virtual void GetVertices(
 		DirectX::XMFLOAT3* vertices, std::uint32_t vertexCount
-	) noexcept = 0;
+	) const noexcept = 0;
 	virtual void GetIndices(
 		std::uint16_t* indices, std::uint32_t indexCount
-	) noexcept = 0;
+	) const noexcept = 0;
 	virtual DirectX::XMMATRIX GetTransform() const noexcept = 0;
 };
 #endif
