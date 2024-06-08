96f17aea1cf502cd51ee36df4d33067115b74a43
razerx100 <saikatmandal8@gmail.com>
2024-06-08 17:37:37 +0100

    Update Camera.hpp

--- app/exports/Camera.hpp
+++ app/exports/Camera.hpp
@@ -8,10 +8,15 @@ class Camera
 {
 public:
 	[[nodiscard]]
-	virtual std::uint32_t GetFov() const noexcept = 0;
-	[[nodiscard]]
+	// The could probably change every frame, so we can just return it. As it is generated
+	// by call to a different function, due to copy elision there won't be unnecessary copies.
 	virtual DirectX::XMMATRIX GetViewMatrix() const noexcept = 0;
 	[[nodiscard]]
-	virtual DirectX::XMMATRIX GetProjectionMatrix() const noexcept = 0;
+	// The projection matrix won't be updated much. So, returning it every frame and then copying
+	// would result in an extra useless copy. So, it would be better to just pass the buffer pointer
+	// and let this class make the copy. Also, makes sure address have enough so the size of
+	// an XMMATRIX can be written.
+	virtual void GetProjectionMatrix(void* address) const noexcept = 0;
+	// No need to return the FOV, as that will be used to calculate the Projection matrix.
 };
 #endif
