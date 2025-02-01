97586eb06f82dfadfb8cb8564f7be708df7c7ed3
razerx100 <saikatmandal8@gmail.com>
2025-02-01 10:39:02 +0000

    Move ReusableVector to Callisto

--- app/includes/GraphicsTechniqueExtensionBase.hpp
+++ app/includes/GraphicsTechniqueExtensionBase.hpp
@@ -3,26 +3,6 @@
 #include <optional>
 #include <GraphicsTechniqueExtension.hpp>
 
-constexpr size_t operator"" _B(unsigned long long number) noexcept
-{
-	return static_cast<size_t>(number);
-}
-
-constexpr size_t operator"" _KB(unsigned long long number) noexcept
-{
-	return static_cast<size_t>(number * 1024u);
-}
-
-constexpr size_t operator"" _MB(unsigned long long number) noexcept
-{
-	return static_cast<size_t>(number * 1024u * 1024u);
-}
-
-constexpr size_t operator"" _GB(unsigned long long number) noexcept
-{
-	return static_cast<size_t>(number * 1024u * 1024u * 1024u);
-}
-
 class GraphicsTechniqueExtensionBase : public GraphicsTechniqueExtension
 {
 public:
