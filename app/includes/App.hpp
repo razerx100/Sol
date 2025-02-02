ead29fcaa778c78a7c3c8918f2dd2ef760ab4293
razerx100 <saikatmandal8@gmail.com>
2025-02-02 10:45:55 +0000

    Update App.hpp

--- app/includes/App.hpp
+++ app/includes/App.hpp
@@ -2,21 +2,24 @@
 #define APP_HPP_
 #include <Renderer.hpp>
 #include <InputManager.hpp>
+#include <ExtensionManager.hpp>
 
 class App
 {
 public:
-	App(Renderer* renderer, InputManager* inputManager, RenderEngineType engineType)
-		: m_renderer{ renderer }, m_inputManager{ inputManager }, m_engineType{ engineType }
-	{}
+	App(
+		Renderer* renderer, InputManager* inputManager, RenderEngineType engineType,
+		ExtensionManager* extensionManager, std::uint32_t frameCount
+	);
 
 	void Init();
 
 	void PhysicsUpdate();
 
 private:
-	Renderer*        m_renderer;
-	InputManager*    m_inputManager;
-	RenderEngineType m_engineType;
+	Renderer*                 m_renderer;
+	BlinnPhongLightTechnique* m_blinnPhong;
+	InputManager*             m_inputManager;
+	RenderEngineType          m_engineType;
 };
 #endif
